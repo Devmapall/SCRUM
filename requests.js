278b0e6d505d492e4dd61ba78fbc18fd3870e8b7
Devmapall <marcel@h2367271.stratoserver.net>
2014-11-20 09:48:46 +0100

    init

---
+++ requests.js
@@ -0,0 +1,51 @@
+function loadProjects() {
+
+}
+
+function loadOverview() {
+
+}
+
+function loadOpenTasks() {
+
+}
+
+function loadOpenIssues() {
+
+}
+
+function loadMyTasks() {
+
+}
+
+function loadMyIssues() {
+
+}
+
+function loadClosedTasks() {
+
+}
+
+function closedIssues() {
+
+}
+
+function addIssue() {
+
+}
+
+function addTask() {
+
+}
+
+function addProject() {
+
+}
+
+function showUsers() {
+
+}
+
+function addUser() {
+
+}
